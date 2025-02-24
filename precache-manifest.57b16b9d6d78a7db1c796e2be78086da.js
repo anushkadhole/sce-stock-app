64125893946ba71b2866b31aa82f5f275f5df425
anq1790 <dholeanushka9@gmail.com>
2025-02-23 17:29:37 -0800

    Updates

---
+++ precache-manifest.57b16b9d6d78a7db1c796e2be78086da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3c2aa30b4e7c55d03df89d7383e896",
+    "url": "/sce-stock-app/index.html"
+  },
+  {
+    "revision": "6987be700f579ee4a9d3",
+    "url": "/sce-stock-app/static/css/main.92f5bc5c.chunk.css"
+  },
+  {
+    "revision": "9eec106de44cf39a97d8",
+    "url": "/sce-stock-app/static/js/2.f402cf16.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/sce-stock-app/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "6987be700f579ee4a9d3",
+    "url": "/sce-stock-app/static/js/main.9bf1eb21.chunk.js"
+  },
+  {
+    "revision": "d32bd062659aeb500a11",
+    "url": "/sce-stock-app/static/js/runtime~main.9682affe.js"
+  }
+]);
